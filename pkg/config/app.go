ad9d94b8728f95020d328be85d9986525f7c3a99
syandi <sandianugrah96@gmail.com>
2021-11-27 11:09:41 +0700

    use postgres

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -2,7 +2,8 @@ package config
 
 import (
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
+	// _ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
 
 var (
@@ -10,7 +11,8 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar?parseTime=true")
+	// d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar?parseTime=true")
+	d, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=belajar sslmode=disable password=postgres")
 	if err != nil {
 		panic(err)
 	}
