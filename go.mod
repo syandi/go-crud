b76149b0101a791f223ba17f83fbd82d3de95357
syandi <sandianugrah96@gmail.com>
2021-11-27 12:10:40 +0700

    environtment

--- go.mod
+++ go.mod
@@ -3,10 +3,16 @@ module crud
 go 1.13
 
 require (
+	github.com/badoux/checkmail v1.2.1
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/go-playground/validator/v10 v10.9.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.4.0
+	github.com/lib/pq v1.10.4 // indirect
+	github.com/rs/cors v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97
 	gopkg.in/validator.v2 v2.0.0-20210331031555-b37d688a7fb0
 )
--- pkg/config/app.go
+++ pkg/config/app.go
@@ -1,9 +1,14 @@
 package config
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/jinzhu/gorm"
-	// _ "github.com/jinzhu/gorm/dialects/mysql"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 )
 
 var (
@@ -11,12 +16,29 @@ var (
 )
 
 func Connect() {
-	// d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar?parseTime=true")
-	d, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=belajar sslmode=disable password=postgres")
+	var err error = godotenv.Load(os.ExpandEnv("../../.env"))
 	if err != nil {
-		panic(err)
+		log.Fatalf("Error getting env %v\n", err)
 	}
-	db = d
+	Dbdriver := os.Getenv("DB_DRIVER")
+	if Dbdriver == "mysql" {
+		// d, err := gorm.Open("mysql", "root:@tcp(127.0.0.1:3306)/belajar?parseTime=true")
+		DBURL := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+		d, err := gorm.Open(Dbdriver, DBURL)
+		if err != nil {
+			panic(err)
+		}
+		db = d
+	}
+	if Dbdriver == "postgres" {
+		// d, err := gorm.Open("postgres", "host=localhost port=5432 user=postgres dbname=belajar sslmode=disable password=postgres")
+		DBURL := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("DB_PASSWORD"))
+		d, err := gorm.Open(Dbdriver, DBURL)
+		if err != nil {
+			panic(err)
+		}
+		db = d
+	}
 }
 
 func GetDB() *gorm.DB {
